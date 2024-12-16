48f87fc254d1121ea16b697ff70e5a90c70fad0a
student <melisaqq55@gmail.com>
2024-12-16 14:42:03 +0800

    2

--- app/src/main/java/com/example/puzzlegame/MainActivity.kt
+++ app/src/main/java/com/example/puzzlegame/MainActivity.kt
@@ -19,12 +19,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PuzzleGameTheme {
+                /*
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+
+                 */
+                SlidingNumberPuzzleGame()
             }
         }
     }
